64e7254fda00b82780c6c3c5771d0cd3715963cd
DarrelRamasray <ddramasr@syr.edu>
2026-09-05 20:04:12 -0400

    Updated directory

--- HWManager.py
+++ HWManager.py
@@ -4,8 +4,8 @@
 
 import streamlit as st
 
-hw1_page = st.Page("HW1.py", title="HW1", icon=":material/description:")
-hw2_page = st.Page("HW2.py", title="HW2", icon=":material/description:", default=True)  #Default page
+hw1_page = st.Page("HW/HW1.py", title="HW1", icon=":material/description:")
+hw2_page = st.Page("HW/HW2.py", title="HW2", icon=":material/description:", default=True)  #Default page
 
 pg = st.navigation([hw2_page, hw1_page])  #HW2 listed first so it appears at the top of the sidebar
 st.set_page_config(page_title="HW Manager", page_icon=":material/edit:")
