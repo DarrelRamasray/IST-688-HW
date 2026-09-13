06b52c756b4651f6e18532ea4dce88d580b644c7
DarrelRamasray <ddramasr@syr.edu>
2026-09-13 16:53:27 -0400

    HW3 added

--- HWManager.py
+++ HWManager.py
@@ -5,8 +5,9 @@
 import streamlit as st
 
 hw1_page = st.Page("HW/HW1.py", title="HW1", icon=":material/description:")
-hw2_page = st.Page("HW/HW2.py", title="HW2", icon=":material/description:", default=True)  #Default page
+hw2_page = st.Page("HW/HW2.py", title="HW2", icon=":material/description:")
+hw3_page = st.Page("HW/HW3.py", title="HW3", icon=":material/description:", default=True)  #Default page
 
-pg = st.navigation([hw2_page, hw1_page])  #HW2 listed first so it appears at the top of the sidebar
+pg = st.navigation([hw3_page, hw2_page, hw1_page])  #HW3 listed first so it appears at the top of the sidebar
 st.set_page_config(page_title="HW Manager", page_icon=":material/edit:")
 pg.run()
